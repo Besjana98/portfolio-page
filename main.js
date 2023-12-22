a78395a544df31618599ba948eb7dfa5cf28b857
DESKTOP-4LP0RSR\besja <besjana101@gmail.com>
2023-12-22 22:43:07 +0100

    Finish

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector("form");
+    var successMessage = document.getElementById("form-message-success");
 
-const app = express();
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Verhindert die tatsächliche Übermittlung des Formulars
 
-// Body Parser Middleware, um Formulardaten zu verarbeiten
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/send-form', (req, res) => {
-    // Hier würden Sie normalerweise die Daten in einer Datenbank speichern
-    // oder eine E-Mail senden. Im Beispiel loggen wir die Daten einfach auf der Konsole.
-    console.log('Name: ' + req.body.name);
-    console.log('Email: ' + req.body.email);
-    console.log('Message: ' + req.body.message);
-    
-    // Sie könnten dann eine Antwort an den Client zurücksenden
-    // Zum Beispiel eine Bestätigungsseite oder eine Nachricht
-    res.send('Formular wurde empfangen!');
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+        // Zeigen Sie die Erfolgsmeldung an und setzen Sie das Formular zurück
+        successMessage.style.display = "block";
+        form.reset();
+    });
 });
