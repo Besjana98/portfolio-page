f6e5c885faa4248de32173832fded92e3714a9fd
DESKTOP-4LP0RSR\besja <besjana101@gmail.com>
2023-12-22 13:42:20 +0100

    structure

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Funktion, um eine Willkommensnachricht anzuzeigen
-    function showWelcomeMessage() {
-        alert("Willkommen auf Besjana Latifi's Portfolio-Seite!");
-    }
-
-    // Button-Event-Listener hinzufügen
-    var welcomeButton = document.getElementById("welcomeButton");
-    if (welcomeButton) {
-        welcomeButton.addEventListener("click", showWelcomeMessage);
-    }
-});
+<script src="script.js"></script>
